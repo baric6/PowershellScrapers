49bbb622160858694ad47733fb8c17f30657c200
baric <31894871+baric6@users.noreply.github.com>
2023-07-25 13:28:45 -0400

    file

---
+++ Prefetch.ps1
@@ -0,0 +1,20 @@
+﻿$sourceFolder = "C:\Windows\Prefetch"
+$destinationFolder = "C:\Users\J.Hollon\Desktop\pf"
+
+$prefetchFiles = Get-ChildItem -Path $sourceFolder -Filter "*.pf" -Recurse
+
+foreach ($file in $prefetchFiles) {
+    $prefetchInfo = Get-ItemProperty -Path $file.FullName -ErrorAction SilentlyContinue
+    if ($prefetchInfo) {
+        Write-Host "Copying Prefetch File: $($file.Name)"
+        Write-Host "Size: $($prefetchInfo.Length) bytes"
+        Write-Host "Creation Time: $($prefetchInfo.CreationTime)"
+        Write-Host "Last Access Time: $($prefetchInfo.LastAccessTime)"
+        Write-Host "Last Write Time: $($prefetchInfo.LastWriteTime)"
+        Write-Host "----------------------------------------"
+
+        $destinationPath = Join-Path $destinationFolder $file.Name
+        Copy-Item -Path $file.FullName -Destination $destinationPath -Force
+    }
+}
+
